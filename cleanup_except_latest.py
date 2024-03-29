df6fe9a3ab087d17b2c4f03c94b54c11c1f3b89a
dyedd <1176996982@qq.com>
2024-03-29 16:24:32 +0800

    feat: combine parameter without edit

--- cleanup_except_latest.py
+++ cleanup_except_latest.py
@@ -1,15 +1,19 @@
 import os
 import shutil
+import argparse
+from utils import load_training_config
 
-def cleanup_except_latest(directory_path):
+def cleanup_except_latest(cfg):
+    directory_path = cfg.output_dir
     # 读取 latest 文件的内容
     latest_file_path = os.path.join(directory_path, "latest")
     with open(latest_file_path, "r") as file:
         # 读取要保留的文件夹名称
         folders_to_keep = file.read().strip().split('\n')
 
-    # 添加"latest"到保留列表
+    # 添加到保留列表
     folders_to_keep.append("latest")
+    folders_to_keep.append(cfg.output_dir + '/' + cfg.ckpt_name)
 
     # 获取目录下的所有文件和文件夹
     all_items = os.listdir(directory_path)
@@ -30,4 +34,11 @@ def cleanup_except_latest(directory_path):
 
 
 if __name__ == '__main__':
-    cleanup_except_latest("/home/dcuuser/dxm/diffusers/train/FineTunedStableDiffusion-lora")
+    parser = argparse.ArgumentParser(description='清理权重空间脚本')
+    parser.add_argument('--cfg', type=str, default="./cfg.json", help='配置文件路径')
+    args = parser.parse_args()
+    cfg_path = args.cfg
+    cfg = load_training_config(cfg_path)
+    if cfg.use_lora.action:
+        cfg.output_dir = cfg.output_dir + "-lora"
+    cleanup_except_latest(cfg)
